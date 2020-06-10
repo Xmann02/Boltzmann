71e78cb33694b836ee0389e4a78acbf0a6e3e490
delsc <delsc@HP8570w.fritz.box>
2020-06-10 18:58:07 +0200

    Saving/Loading networks included, generally cleaned up and some options implemented

---
+++ RBM.m
@@ -0,0 +1,285 @@
+%Programm by Xmann02 and DontStealMyAccount
+%
+
+%% Global parameters
+
+prop.sizeH = 2000; %size of the h-vector
+prop.startLearningRate = 0.01; %learning rate
+prop.endLearningRage = 0.001; %
+prop.numTrainingImages = 1000; % number of images in training routine
+prop.numGibbsIterations = 50; %number of iterations of gibbs sample
+prop.gibbsSampleType = 'Propability'; % options 'Reconstruct', 'Propability'
+prop.gibbsSampleInput = 'Sample'; %options: 'Sample', 'Random' NOT YET IMPLEMENTED
+prop.regularizer = 'L2'; %reguarlizer options: 'None', 'L1', L2' 
+prop.regularizerLambda = 0.0001; %regularizer Lambda 
+prop.imageType = 'Grayscale'; %options: 'Grayscale', 'BW' 
+prop.imageSamples = '4'; %options: 'All' or any single digit 
+prop.trainingPercentage = 0.8; %percentage of data used for training
+
+funs = iniFunctions(prop);
+
+
+% Image preparation
+
+[images,labels] = prepareTrainingData(prop);
+
+
+% initialization
+[a,b,W] = ini(images,prop.sizeH);
+%[a,b,W,prop] = loadNetwork(3);
+
+
+
+% Working area
+[a,b,W] = trainNetwork(images,a,b,W,prop,funs);
+
+
+% Save Network
+%saveNetwork(a,b,W,prop);
+
+
+% Show Samples
+plotSamples(images,a,b,W,prop,funs);
+
+
+
+
+%% Functions 
+
+%% initialize functions
+function funs = iniFunctions(prop)
+    
+    if(strcmp(prop.gibbsSampleType,'Reconstruct'))
+        funs.gibbsSample = @(vData,a,b,W,prop)GibbsSampleReconstruct(vData,a,b,W,prop);
+    end
+    if(strcmp(prop.gibbsSampleType,'Propability'))
+        funs.gibbsSample = @(vData,a,b,W,prop)GibbsSamplePropability(vData,a,b,W,prop);
+    end
+    
+    if(strcmp(prop.regularizer,'None'))
+        funs.updateNetwork = @(a,b,W,vData,pVj,pHjData,pHj,prop)updateNoRegularizer(a,b,W,vData,pVj,pHjData,pHj,prop);
+    end    
+    if(strcmp(prop.regularizer,'L1'))
+        funs.updateNetwork = @(a,b,W,vData,pVj,pHjData,pHj,prop)updateL1(a,b,W,vData,pVj,pHjData,pHj,prop);
+    end 
+    if(strcmp(prop.regularizer,'L2'))
+        funs.updateNetwork = @(a,b,W,vData,pVj,pHjData,pHj,prop)updateL2(a,b,W,vData,pVj,pHjData,pHj,prop);
+    end 
+    
+
+end
+
+%% Plot 
+function plotSamples(images,a,b,W,prop,funs)
+figure(1)
+for i=1:16
+subplot(4,8,2*i-1)
+A = images(:,:,randi([round(prop.trainingPercentage * length(images)),round(length(images))],1,1));
+v = vectorizeImage(A,28,28);
+imshow(A)
+subplot(4,8,2*i)
+%v = rand(28*28,1);
+%sample an image to generate new sample
+[pVj,pHj] = funs.gibbsSample(v,a,b,W,prop);
+if strcmp(prop.imageType,'BW')
+    v = pVj > rand(28*28,1);
+    A = reconstructImage(v,28,28);
+end    
+if strcmp(prop.imageType,'Grayscale')
+    A = reconstructImage(pVj,28,28);
+end    
+imshow(A)
+end    
+end
+
+
+%% Training loop
+
+function [a,b,W] = trainNetwork(images,a,b,W,prop,funs)
+
+%Training Loop
+for j=1:prop.numTrainingImages
+j    
+%alpha = 1/(100+j/10);
+%use this to train network for specific number, needs to be removed later
+%if labels(j)==4
+A = images(:,:,randi([1,round(prop.trainingPercentage * length(images))],1,1));
+[a,b,W] = trainingStep(A,a,b,W,prop,funs);  
+prop.learningRate = prop.startLearningRate*(1-j/prop.numTrainingImages) + prop.endLearningRage*j/prop.numTrainingImages;
+%end    
+
+end
+end
+
+
+%% image preparation
+function [images,labels] = prepareTrainingData(prop)
+
+[images, labels] = mnist_parse('train-images.idx3-ubyte','train-labels.idx1-ubyte');
+
+if strcmp(prop.imageType,'BW')
+    %Simple cutoff
+    images = (images > 120);
+else if strcmp(prop.imageType,'Grayscale')
+    %image in grayscale
+    images = double(images)/255;
+    else
+        disp('Mistake in prop.imageType');
+    end
+end        
+
+if ~strcmp(prop.imageSamples,'All')
+    mask = (labels == str2num(prop.imageSamples));
+    images = images(:,:,mask);
+end    
+
+end
+
+
+%% saving and loading trained networks
+function saveNetwork(a,b,W,prop)
+disp('saving network')
+for i=1:10
+    if ~exist(strcat("RBMs/", int2str(i)), 'dir')
+        mkdir(strcat("RBMs/", int2str(i)))
+        break;
+    end
+end    
+
+save(strcat(strcat("RBMs/",int2str(i)),"/a.mat"),"a")
+save(strcat(strcat("RBMs/",int2str(i)),"/b.mat"),"b")
+save(strcat(strcat("RBMs/",int2str(i)),"/W.mat"),"W")
+save(strcat(strcat("RBMs/",int2str(i)),"/prop.mat"),"prop")
+
+end
+
+function [a,b,W,prop] = loadNetwork(i)
+disp('loading network')
+a = matfile(strcat(strcat("RBMs/",int2str(i)),"/a.mat"));
+b = matfile(strcat(strcat("RBMs/",int2str(i)),"/b.mat"));
+W = matfile(strcat(strcat("RBMs/",int2str(i)),"/W.mat"));
+prop = matfile(strcat(strcat("RBMs/",int2str(i)),"/prop.mat"));
+
+a = a.a;
+b = b.b;
+W = W.W;
+prop = prop.prop
+
+end
+
+
+%% Initialization values 
+function [a,b,W] = ini(images,sizeH)
+disp("initalizing Parameters ...");
+%Get sizes of dataset
+imageSize = size(images(:,:,1));
+imageNumber = length(images(1,1,:));
+
+%Random initialization of W matrix
+%Values from Mehta p 98 on the bottom
+W = normrnd(0,2/sqrt(sizeH+imageSize(1)*imageSize(2)),[sizeH,imageSize(1)*imageSize(2)]);
+
+%Random intialization for input bias vector
+%Values from Mehta p 98 on the bottom
+initalizationInputBiasSigma = 1/mean(mean(mean(images)));
+a = normrnd(0,initalizationInputBiasSigma,[imageSize(1)*imageSize(2),1]);
+
+%Zero initialization for hidden layer bias vector
+b = zeros(sizeH,1);
+
+a = gpuArray(a);
+b = gpuArray(b);
+W = gpuArray(W);
+
+disp("Done")
+end
+
+%% Do training step on sample image
+
+function [a,b,W] = trainingStep(image,a,b,W,prop,funs)
+
+vData = gpuArray(vectorizeImage(image,28,28));
+pHjData = gpuArray(1./(1+arrayfun(@(x)exp(-x), b + W*vData)));
+%pHj = pHjData;
+%hData = gpuArray(rand(size(pHj))<pHj);
+
+    
+[pVj,pHj] = funs.gibbsSample(vData,a,b,W,prop);    
+
+%Update Network parameters
+[a,b,W] = funs.updateNetwork(a,b,W,vData,pVj,pHjData,pHj,prop);
+
+end
+
+function [a,b,W] = updateNoRegularizer(a,b,W,vData,pVj,pHjData,pHj,prop)
+a = a + prop.learningRate * (vData-pVj);
+b = b + prop.learningRate * (pHjData-pHj);
+W = W + prop.learningRate * (kron(pHjData,vData')-kron(pHj,pVj'));
+end
+
+function [a,b,W] = updateL1(a,b,W,vData,pVj,pHjData,pHj,prop)
+a = a + prop.learningRate * (vData-pVj);
+b = b + prop.learningRate * (pHjData-pHj);
+W = W + prop.learningRate * (kron(pHjData,vData')-kron(pHj,pVj')) - prop.regularizerLambda * sign(W) ;
+end
+
+function [a,b,W] = updateL2(a,b,W,vData,pVj,pHjData,pHj,prop)
+a = a + prop.learningRate * (vData-pVj);
+b = b + prop.learningRate * (pHjData-pHj);
+W = W + prop.learningRate * (kron(pHjData,vData')-kron(pHj,pVj')) - 2 * prop.regularizerLambda * W ;
+end
+
+
+%% Calculate a gibbs sample
+%input vector from data, biases for visible/hidden layer, interaction
+%Matrix, num iterations
+%Returns propability vectors for v and h
+function [pVj,pHj] = GibbsSampleReconstruct(vData,a,b,W,prop) 
+vModel = gpuArray(vData);
+for i=1:prop.numGibbsIterations
+pHj = gpuArray(1./(1+arrayfun(@(x)exp(-x), b + W*vModel)));
+hModel = gpuArray(rand(size(pHj))<pHj);
+
+pVj = gpuArray(1./(1+arrayfun(@(x)exp(-x), a + W'*hModel)));
+vModel = gpuArray(rand(size(pVj))<pVj);
+end    
+
+end
+
+function [pVj,pHj] = GibbsSamplePropability(vData,a,b,W,prop) 
+vModel = gpuArray(vData);
+pHj = gpuArray(1./(1+arrayfun(@(x)exp(-x), b + W*vModel)));
+pVj = gpuArray(1./(1+arrayfun(@(x)exp(-x), a + W'*pHj)));
+
+for i=1:prop.numGibbsIterations
+pHj = gpuArray(1./(1+arrayfun(@(x)exp(-x), b + W*pVj)));
+pVj = gpuArray(1./(1+arrayfun(@(x)exp(-x), a + W'*pHj)));
+end    
+
+end
+
+
+%% Function to calculate sigmoid function
+% Not used, because it is incompatible with gpu computing
+function sigmoidCustom = sigmoidCustom(x)
+sigmoidCustom = 1./(1+arrayfun(@(x)exp(-x),x));
+end
+
+%% Calculate Energy from vector
+function energy = energy(x,h,a,b,W) %input Vector, hidden layer Vector, input bias, hidden layer Bias, exchange Matrix W
+energy = -h'*W*x-a'*x-b'*h;
+end
+
+
+%% Reshape image to vector
+function vectorX = vectorizeImage(image,sizeX,sizeY)
+vectorX = reshape(image,[sizeX*sizeY,1]);
+end
+
+%% Reconstruct image from a vector
+function image = reconstructImage(vector,sizeX,sizeY)
+image = reshape(vector,[sizeX,sizeY]);
+end
+
+
+
--- gpuTesting.m
+++ gpuTesting.m
@@ -1,15 +1,43 @@
-gpuDevice
+gpuDevice;
 
-a = linspace(1,10,10)
-b = linspace(11,20,10)
+a = linspace(1,10,10);
+b = linspace(11,20,10);
 
-aGpu = gpuArray(a)
-bGpu = gpuArray(b)
+aGpu = gpuArray(a);
+bGpu = gpuArray(b);
 
-c = arrayfun(@(x)arrayTest(x),a)  %1./(1-arrayfun(@(x)exp(-x),a))
-cGpu = arrayfun(@(x)arrayTest(x),aGpu) %1./(1-arrayfun(@(x)exp(-x),aGpu))
+%c = arrayfun(@(x)arrayTest(x),a)  %1./(1-arrayfun(@(x)exp(-x),a))
+%cGpu = arrayfun(@(x)arrayTest(x),aGpu) %1./(1-arrayfun(@(x)exp(-x),aGpu))
+
+x = linspace(1,10,10)
+x = gpuArray(x);
+
+option = 2
+
+if(option == 1)
+        fun = @(x)deepFunction1(x);
+    end    
+    if(option == 2)
+        fun = @(x)deepFunction2(x);
+    end
+
+y = surfaceFun1(x,fun)
 
 
 function arrayTest = arrayTest(x)
 arrayTest = 1/(1+arrayfun(@(x)exp(-x),x));
+end
+
+function surfaceFun1 = surfaceFun1(x,fun)
+    surfaceFun1 = fun(x);
+end
+
+function deepFunction1 = deepFunction1(x)
+    disp('Deep function 1')
+    deepFunction1 = x;
+end
+
+function deepFunction2 = deepFunction2(x)
+    disp('Deep function 2')
+    deepFunction2 = x.^2;
 end
